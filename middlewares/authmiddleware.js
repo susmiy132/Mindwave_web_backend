84d97522f832cc97aed598d6f33ae072db8d13cc
USER <thakursusmi94@gmail.com>
2025-06-13 20:50:05 +0545

    generate token

--- middlewares/authmiddleware.js
+++ middlewares/authmiddleware.js
@@ -3,17 +3,17 @@ const jwt = require('jsonwebtoken');
 const authMiddleware = (req, res, next) => {
   const token = req.header("Authorization")?.split(" ")[1]; // "Bearer <token>"
 
-//   if (!token) {
-//     return res.status(401).json({ message: "No token, authorization denied" });
-//   }
+  if (!token) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
 
-//   try {
-//     const decoded = jwt.verify(token, process.env.SECRET);
-//     req.user = decoded.id;
-//     next();
-//   } catch (err) {
-//     return res.status(401).json({ message: "Token is not valid" });
-//   }
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.user = decoded.id;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Token is not valid" });
+  }
 };
 
 module.exports = authMiddleware;
