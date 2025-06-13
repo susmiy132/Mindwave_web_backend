fff22260526da08c602e929a4ffdae5ab7ebbcf9
USER <thakursusmi94@gmail.com>
2025-06-13 20:38:50 +0545

    added user model

--- models/User.js
+++ models/User.js
@@ -1,20 +1,20 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const userSchema = new mongoose.Schema({
-//   fullName: {
-//     type: String,
-//     required: true,
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//   },
-// });
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-// const User = mongoose.model('User', userSchema);
-// module.exports = User;
+const User = mongoose.model('User', userSchema);
+module.exports = User;
