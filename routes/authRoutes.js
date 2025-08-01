4c6c11541c2eeb733b0b0721823ee4ad4e9e0d88
USER <thakursusmi94@gmail.com>
2025-08-01 12:34:32 +0545

    added authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,18 +1,18 @@
 
-// const express = require('express');
-// const router = express.Router();
-// const authController = require('../controllers/authController');
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
 
-// // for test
-// const authMiddleware = require("../middlewares/authmiddleware");
+// for test
+const authMiddleware = require("../middlewares/authmiddleware");
 
-// router.get("/me", authMiddleware, (req, res) => {
-//   res.status(200).json(req.user); // send back the authenticated user's data
-// });
-// // POST /api/auth/register
+router.get("/me", authMiddleware, (req, res) => {
+  res.status(200).json(req.user); // send back the authenticated user's data
+});
+// POST /api/auth/register
 // router.post('/register', authController.register);
 
-// // POST /api/auth/login
+// POST /api/auth/login
 // router.post('/login', authController.login);
 
-// module.exports = router;
+module.exports = router;
