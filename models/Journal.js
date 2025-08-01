da31e74a462e6dd536daf9150f0d24758ddbb2ae
USER <thakursusmi94@gmail.com>
2025-08-01 12:55:49 +0545

    added journal model

--- models/Journal.js
+++ models/Journal.js
@@ -1,12 +1,12 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const journalSchema = new mongoose.Schema({
-//   title: { type: String, required: true },
-//   content: { type: String, required: true },
-//   mood: { type: String }, // e.g. "happy", "sad", "neutral"
-//   image: { type: String }, // optional image for journal entry
-//   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" }, // admin or user reference
-//   createdAt: { type: Date, default: Date.now },
-// });
+const journalSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  mood: { type: String }, // e.g. "happy", "sad", "neutral"
+  image: { type: String }, // optional image for journal entry
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" }, // admin or user reference
+  createdAt: { type: Date, default: Date.now },
+});
 
-// module.exports = mongoose.model("Journal", journalSchema);
+module.exports = mongoose.model("Journal", journalSchema);
