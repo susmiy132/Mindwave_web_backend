5e88dbe7ea72fd246fec3ab4ac0100401defd38e
USER <thakursusmi94@gmail.com>
2025-08-01 12:30:09 +0545

    added nodemailer

--- controllers/authController.js
+++ controllers/authController.js
@@ -141,15 +141,15 @@ exports.login = async (req, res) => {
 };
 
 
-// const transporter = nodemailer.createTransport(
-//   {
-//     service: "gmail",
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS
-//     }
-//   }
-// )
+const transporter = nodemailer.createTransport(
+  {
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  }
+)
 
 // exports.sendResetLink = async = async (req, res) => {
 //     const { email } = req.body
