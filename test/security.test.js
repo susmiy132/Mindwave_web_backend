8dfae39cedfe330e1d111c39ce7de08c9b464ac2
USER <thakursusmi94@gmail.com>
2025-08-01 15:21:03 +0545

    added security testing

--- test/security.test.js
+++ test/security.test.js
@@ -1,15 +1,15 @@
-// const { app, request } = require("./setup");
+const { app, request } = require("./setup");
 
-// describe("Security & Auth Failures", () => {
-//   test("Access journals without token", async () => {
-//     const res = await request(app).get("/api/journals");
-//     expect(res.statusCode).toBe(401);
-//   });
+describe("Security & Auth Failures", () => {
+  test("Access journals without token", async () => {
+    const res = await request(app).get("/api/journals");
+    expect(res.statusCode).toBe(401);
+  });
 
-//   test("Access mood with invalid token", async () => {
-//     const res = await request(app)
-//       .get("/api/mood")
-//       .set("Authorization", "Bearer faketoken");
-//     expect(res.statusCode).toBe(403);
-//   });
-// });
+  test("Access mood with invalid token", async () => {
+    const res = await request(app)
+      .get("/api/mood")
+      .set("Authorization", "Bearer faketoken");
+    expect(res.statusCode).toBe(403);
+  });
+});
