acd3ed1b4778f0b4bad8c60ee35adb7f77233725
USER <thakursusmi94@gmail.com>
2025-08-01 13:12:42 +0545

    added mood controller

--- controllers/moodController.js
+++ controllers/moodController.js
@@ -1,48 +1,48 @@
-// // const Mood = require('../models/Mood');
+// const Mood = require('../models/Mood');
 
-// // // POST /api/moods
-// // exports.recordMood = async (req, res) => {
-// //   const { mood } = req.body;
-// //   if (!mood) return res.status(400).json({ message: 'Mood is required' });
+// // POST /api/moods
+// exports.recordMood = async (req, res) => {
+//   const { mood } = req.body;
+//   if (!mood) return res.status(400).json({ message: 'Mood is required' });
 
-// //   try {
-// //     const newMood = await Mood.create({
-// //       userId: req.user._id,
-// //       mood,
-// //     });
-
-// //     res.status(201).json(newMood);
-// //   } catch (err) {
-// //     res.status(500).json({ message: err.message });
-// //   }
-// // };
-
-// // // GET /api/moods
-// // exports.getMoods = async (req, res) => {
-// //   try {
-// //     const moods = await Mood.find({ userId: req.user._id }).sort({ date: -1 });
-// //     res.json(moods);
-// //   } catch (err) {
-// //     res.status(500).json({ message: err.message });
-// //   }
-// // };
-
-
-// const Mood = require("../models/Mood");
-
-// exports.createMood = async (req, res) => {
 //   try {
-//     const { mood } = req.body;
-//     const newMood = new Mood({
+//     const newMood = await Mood.create({
+//       userId: req.user._id,
 //       mood,
-//       user: req.user ? req.user.id : null,
 //     });
-//     await newMood.save();
+
 //     res.status(201).json(newMood);
 //   } catch (err) {
-//     res.status(500).json({ message: "Failed to save mood" });
+//     res.status(500).json({ message: err.message });
 //   }
 // };
+
+// // GET /api/moods
+// exports.getMoods = async (req, res) => {
+//   try {
+//     const moods = await Mood.find({ userId: req.user._id }).sort({ date: -1 });
+//     res.json(moods);
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// };
+
+
+const Mood = require("../models/Mood");
+
+exports.createMood = async (req, res) => {
+  try {
+    const { mood } = req.body;
+    const newMood = new Mood({
+      mood,
+      user: req.user ? req.user.id : null,
+    });
+    await newMood.save();
+    res.status(201).json(newMood);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to save mood" });
+  }
+};
 
 // exports.getMoods = async (req, res) => {
 //   try {
