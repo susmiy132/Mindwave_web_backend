a07bd9033c7812c96605912141dfb7b514cdb671
USER <thakursusmi94@gmail.com>
2025-08-01 13:30:51 +0545

    added createNotice

--- controllers/noticeController.js
+++ controllers/noticeController.js
@@ -1,16 +1,16 @@
 // controllers/notice.controller.js
 const Notice = require('../models/Notice');
 
-// // CREATE
-// exports.createNotice = async (req, res) => {
-//   try {
-//     const notice = new Notice(req.body);
-//     await notice.save();
-//     res.status(201).json(notice);
-//   } catch (err) {
-//     res.status(400).json({ error: err.message });
-//   }
-// };
+// CREATE
+exports.createNotice = async (req, res) => {
+  try {
+    const notice = new Notice(req.body);
+    await notice.save();
+    res.status(201).json(notice);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
 
 // // READ ALL
 // exports.getNotices = async (req, res) => {
